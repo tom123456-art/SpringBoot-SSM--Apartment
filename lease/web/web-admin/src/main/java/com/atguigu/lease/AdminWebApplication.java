39bfad08109383cf6d4d99ec12e78fc515d668d9
tom123456-art <2470688428@qq.com>
2025-08-02 22:04:26 +0800

    实现支付方式管理接口，配置逻辑删除与自动填充

--- lease/web/web-admin/src/main/java/com/atguigu/lease/AdminWebApplication.java
+++ lease/web/web-admin/src/main/java/com/atguigu/lease/AdminWebApplication.java
@@ -1,11 +1,10 @@
 package com.atguigu.lease;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.atguigu.lease"})
 public class AdminWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminWebApplication.class, args);
