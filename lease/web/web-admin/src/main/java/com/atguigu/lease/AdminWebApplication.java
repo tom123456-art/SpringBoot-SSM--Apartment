b3703f2fdab900c0132a33dc5ae463deb4db8520
tom123456-art <2470688428@qq.com>
2025-08-05 21:05:16 +0800

    实现租赁管理接口及租约定时检查任务

--- lease/web/web-admin/src/main/java/com/atguigu/lease/AdminWebApplication.java
+++ lease/web/web-admin/src/main/java/com/atguigu/lease/AdminWebApplication.java
@@ -2,9 +2,11 @@ package com.atguigu.lease;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication(scanBasePackages = {"com.atguigu.lease"})
+@EnableScheduling
 public class AdminWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminWebApplication.class, args);
